6472a6267ca639ab5cb64a21239b55307b7c4f90
shun123456 <shun_234@163.com>
2019-07-01 11:25:44 +0800

    init project

---
+++ commitlint/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    提交验证
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
